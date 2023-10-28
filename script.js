4752010b9665512ca87f05609375724baaa01893
Josue Tutor <tutorjosue32@gmail.com>
2023-10-29 03:01:30 +0800

    updating code

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const input = document.querySelector('.input');
-const button = document.querySelector('.button');
+const form = document.querySelector('.form');
 const message = document.querySelector('.message');
 const todoItem = document.querySelector('.todoItem');
 
-button.addEventListener('click', addTodo);
+let todos = [];
 
-input.addEventListener('keyup', function(event) {
-    if (event.key === 'Enter') {
+loadTodosFromLocalStorage();
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (input.value.trim() !== '') {
         addTodo();
     }
 });
@@ -17,62 +21,100 @@ todoItem.addEventListener('click', function(event) {
         target.classList.toggle("checked");
         savetoLocalStorage();
     } else if (target.classList.contains('spanCloseButton')) {
-        target.parentElement.remove();
-        message.textContent = messages("Success delete.");
-        setTimeout(() => {
-            message.textContent = messages("");
-        }, 3000);
-
-        savetoLocalStorage();
+        const li = target.parentElement;
+        const todoId = li.getAttribute('data-id');
+        deleteTodoById(todoId);
     } else if (target.classList.contains('edit')) {
-        const taskElement = target.previousElementSibling;
-        const originalText = taskElement.textContent;
-        const inputElement = document.createElement('input');
-        inputElement.value = originalText;
-        taskElement.innerHTML = '';
-        taskElement.appendChild(inputElement);
-        inputElement.focus();
-
-        console.log(taskElement)
-
-        inputElement.addEventListener('blur', function() {
-            taskElement.innerHTML = inputElement.value;
-            savetoLocalStorage();
-        });
-
-        inputElement.addEventListener('keyup', function(event) {
-            if (event.key === "Enter") {
-                taskElement.innerHTML = inputElement.value;
-                savetoLocalStorage();
-            }
-        });
+        editTodoItem(target);
     }
-}, false);
+});
 
 function messages(msg) {
-    return `${msg}`;
+    return msg;
 }
 
 function savetoLocalStorage() {
-    localStorage.setItem("data", todoItem.innerHTML);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadTodosFromLocalStorage() {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+        todos = JSON.parse(storedTodos);
+    }
+}
+
+function removeTodoFromLocalStorage(todoId) {
+    todos = todos.filter(todo => todo.id !== todoId);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function deleteTodoById(todoId) {
+    const li = document.querySelector(`[data-id="${todoId}"]`);
+    if (!li) return;
+
+    const deletedTodo = todos.find(todo => todo.id === todoId);
+    li.remove();
+
+    if (deletedTodo) {
+        displayMessage(messages(`${deletedTodo.text} Successfully deleted.`));
+        removeTodoFromLocalStorage(todoId);
+    }
 }
 
 function showTaskItems() {
-    todoItem.innerHTML =  localStorage.getItem("data");
+    todoItem.innerHTML = '';
+    if (todos.length === 0) {
+        displayMessage(messages('No task available.'));
+    } else {
+        todos.forEach(todo => {
+            const li = document.createElement('li');
+            li.textContent = todo.text;
+            li.classList.add('itemTask');
+            li.setAttribute('data-id', todo.id);
+
+            let editItem = document.createElement('p');
+            editItem.textContent = '✎';
+            editItem.classList.add('edit');
+            li.appendChild(editItem);
+
+            let span = document.createElement('span');
+            span.textContent = '\u00d7';
+            span.classList.add('spanCloseButton');
+            li.appendChild(span);
+
+            todoItem.appendChild(li);
+        });
+    }
 }
 
 showTaskItems();
 
 function addTodo() {
-    if (input.value === '') {
-        message.textContent = messages("Input is required!");
+    const text = input.value.trim();
+
+    if (text === '') {
+        displayMessage(messages('Input is required!'));
         return;
     }
 
-    let li = document.createElement('li');
-    li.textContent = input.value;
+    const taskExists = todos.some(todo => todo.text === text);
+
+    if (taskExists) {
+        displayMessage(messages('Task already exists!'));
+        return;
+    }
+
+    const todoId = `todo_${todos.length}`;
+    todos.push({
+        id: todoId,
+        text,
+        isEditing: false,
+    });
+
+    const li = document.createElement('li');
+    li.textContent = text;
     li.classList.add('itemTask');
-    todoItem.appendChild(li);
 
     let editItem = document.createElement('p');
     editItem.textContent = '✎';
@@ -84,13 +126,74 @@ function addTodo() {
     span.classList.add('spanCloseButton');
     li.appendChild(span);
 
-    message.textContent = messages("New Todo Added.");
-    message.classList.add('success-msg');
+    li.setAttribute('data-id', todoId);
 
-    setTimeout(() => {
-        message.textContent = '';
-    }, 3000);
+    todoItem.appendChild(li);
 
+    displayMessage(messages('New Todo Added.'));
     input.value = '';
     savetoLocalStorage();
+}
+
+function editTodoItem(editButton) {
+    const li = editButton.parentElement;
+    const todoId = li.getAttribute('data-id');
+    const editTodo = todos.find(todo => todo.id === todoId);
+
+    if (editTodo && !editTodo.isEditing) {
+        editTodo.isEditing = true;
+
+        const taskElement = li.childNodes[0];
+
+        if (taskElement) {
+            let inputElement = document.createElement('input');
+            inputElement.value = taskElement.textContent;
+            inputElement.classList.add('editing-input')
+            li.replaceChild(inputElement, taskElement);
+            inputElement.focus();
+
+            let isEditing = true; // Flag to track the editing state
+
+            inputElement.addEventListener('blur', function() {
+                if (isEditing) {
+                    taskElement.textContent = inputElement.value;
+                }
+                editTodo.isEditing = false;
+                isEditing = false;
+            });
+
+            inputElement.addEventListener('keyup', function(event) {
+                if (event.key === "Enter") {
+                    if (isEditing) {
+                        const newTaskText = inputElement.value;
+
+                        if(isDuplicateTask(newTaskText)) {
+                            displayMessage(messages('Task already exists!'));
+                            return;
+                        }
+
+                        taskElement.textContent = inputElement.value;
+                        displayMessage(messages('Edit success.'));
+                        editTodo.text = newTaskText;
+                    }
+                    editTodo.isEditing = false;
+                    li.replaceChild(taskElement, inputElement);
+                    savetoLocalStorage();
+                }
+            });
+        }
+    }
+}
+
+function isDuplicateTask(newTaskText) {
+    return todos.some(todo => todo.text === newTaskText);
+}
+
+
+function displayMessage(msg, timeout = 3000) {
+    message.textContent = msg;
+    message.classList.add('success-msg');
+    setTimeout(() => {
+        message.textContent = '';
+    }, timeout);
 }
